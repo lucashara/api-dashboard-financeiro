ee0209288eb40e994caeffc2e86e506610cd9c04
Lucas Harã <lucashara20@gmail.com>
2023-11-28 09:05:36 -0300

    inclusa rota pcprest

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.exc import SQLAlchemyError
-from config_bd import SessionLocal, text  # Importando a configuração do banco de dados
+from config_bd import SessionLocal, text
 import json
 from datetime import datetime
+from decimal import Decimal
+
 
 app = FastAPI()
 
-# Habilita o CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -22,20 +23,44 @@ def convert_datetime(obj):
     raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
 
 @app.get("/liberacao")
-def read_data():
+def read_liberacao_data():
     query = open("liberacao.sql", "r").read()
+    return execute_query(query)
+
+@app.get("/pcprest")
+def read_pcprest_data():
+    query = open("pcprest.sql", "r").read()
+    return execute_query(query)
+
+def execute_query(query):
     try:
         with SessionLocal() as session:
-            # Use text() para a consulta SQL
             cursor = session.execute(text(query))
             columns = [col[0] for col in cursor.cursor.description]
-            results = [dict(zip(columns, row)) for row in cursor.fetchall()]
+            results = []
+            for row in cursor.fetchall():
+                row_dict = {}
+                for col_name, value in zip(columns, row):
+                    if col_name == 'DATAVENCIMENTO' and isinstance(value, datetime):
+                        row_dict[col_name] = value.strftime('%Y-%m-%d')
+                    elif isinstance(value, Decimal):
+                        row_dict[col_name] = float(value)
+                    else:
+                        row_dict[col_name] = value
+                results.append(row_dict)
+
             return Response(content=json.dumps(results, default=convert_datetime), media_type="application/json")
     except SQLAlchemyError as e:
         print(f"Erro ao acessar o banco de dados: {e}")
         return Response(content=json.dumps({"error": "Erro ao acessar o banco de dados"}), media_type="application/json")
 
+    
+def convert_datetime(obj):
+    if isinstance(obj, datetime):
+        return obj.isoformat()
+    elif isinstance(obj, Decimal):  # Adiciona esta linha para lidar com objetos Decimal
+        return float(obj)  # Ou str(obj) se você preferir representar os decimais como strings
+    raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
 
-# Execute o servidor usando: uvicorn main:app --reload --host 0.0.0.0 --port 8001
-# uvicorn main:app --host 0.0.0.0 --port 8001
 
+# Para executar o servidor use: uvicorn main:app --reload --host 0.0.0.0 --port 8001
